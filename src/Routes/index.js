3595d18545290a38a7a84e8f508a3285669db18a
Rimple07 <rchaudhary@innow8apps.com>
2023-06-06 08:30:18 -0700

    Update index.js

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,14 +1,13 @@
-import { BrowserRoter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "../Components/Home/Home";
-import SignIn from "../Components/Auth/SignIn";
-import SignUp from "../Components/Userinformation/userInformation";
+import SignIn from "../Components/AUth/SignIn";
 
 const Routes = () => {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={SignIn} />
-        <Route exact path="/signup" component={SignUp} />
+        {/* <Route exact path="/signup" component={SignUp} /> */}
         <Route path="/home" component={Home} />
       </Switch>
     </Router>
